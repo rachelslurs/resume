e1b3ca5297a2d3e6889bae98d05169bb7c335e61
rachelslurs <rachelcantor@gmail.com>
2024-04-25 17:16:15 -0400

    fix: styles

--- src/components/Articles/AboutMe.tsx
+++ src/components/Articles/AboutMe.tsx
@@ -5,10 +5,7 @@ import Prose from '../Prose/Prose'
 const AboutMe: React.FC = () => {
   return (
     <>
-      <Prose
-        html={personal.body.html}
-        className="mt-8 max-w-[36rem] xl:max-w-[44rem]"
-      />
+      <Prose html={personal.body.html} className="mt-6 text-xl" />
     </>
   )
 }
